14f84fb485cbda3cd4a265739f12d426b7668e63
Mason McCoy <mmasonmccoy@gmail.com>
2021-01-30 11:11:32 -0600

    Updated SQLAlchemy script. Made changes to CombinedDF.csv

--- SQL/csv-to-sql.py
+++ SQL/csv-to-sql.py
@@ -5,31 +5,31 @@ Created on Fri Jan 22 17:12:14 2021
 @author: Mason
 """
 
-from sqlalchemy import create_engine, Integer, BigInteger, Float, Text, Date
+from sqlalchemy import create_engine, Decimal
 import pandas
 from config import password
 
 engine = create_engine("postgresql://postgres:" + password + "@localhost/weatherCOVID")
 
-longlatDF = pandas.read_csv("data/county_centers.csv", header = 0)
-stateDF = pandas.read_csv("data/us-states.csv", header = 0)
-countyDF = pandas.read_csv("data/us-counties.csv", header = 0)
+longlatDF = pandas.read_csv("./../raw-data/county_centers.csv", header = 0)
+stateDF = pandas.read_csv("./../raw-data/us-states.csv", header = 0)
+countyDF = pandas.read_csv("../raw-data/us-counties.csv", header = 0)
+weatherDF = pandas.read_csv("../combinedDF.csv", header = 0)
+
+print(longlatDF)
+print(stateDF)
+print(countyDF)
+print(weatherDF)
 
 adjustedLonglat = longlatDF.drop(['clon10', 'clat10', 'pclon00', 'pclat00', 'pclon10', 'pclat10'], axis = 1)
 
 
-# print(adjustedLonglat)
-# print(stateDF)
-# print(countyDF)
-
-adjustedLonglat.to_sql("lat_long", con = engine, if_exists = "replace", dtype = {"fips": BigInteger, "clon00": Float,
-                                                                                 "clat00": Float})
+adjustedLonglat.to_sql("lat_long", con = engine, if_exists = "replace")
 engine.execute("SELECT * FROM lat_long").fetchall()
 print("lat_long table updated!")
 
 
-stateDF.to_sql("state", con = engine, if_exists = "replace", dtype = {"date": Date, "state": Text, "fips": Integer,
-                                                                           "cases": BigInteger, "deaths": BigInteger})
+stateDF.to_sql("state", con = engine, if_exists = "replace")
 engine.execute("SELECT * FROM state").fetchall()
 print("state table updated!")
 
