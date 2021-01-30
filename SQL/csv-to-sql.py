c499a6ec4c64a7fc855b5cc84064f80afd02d329
Mason McCoy <mmasonmccoy@gmail.com>
2021-01-30 11:24:23 -0600

    Updated SQLAlchemy script to account for Flask-preferred dtypes. Cleaned up county_centers.csv

--- SQL/csv-to-sql.py
+++ SQL/csv-to-sql.py
@@ -5,7 +5,7 @@ Created on Fri Jan 22 17:12:14 2021
 @author: Mason
 """
 
-from sqlalchemy import create_engine, Decimal
+from sqlalchemy import create_engine, Numeric
 import pandas
 from config import password
 
@@ -16,15 +16,14 @@ stateDF = pandas.read_csv("./../raw-data/us-states.csv", header = 0)
 countyDF = pandas.read_csv("../raw-data/us-counties.csv", header = 0)
 weatherDF = pandas.read_csv("../combinedDF.csv", header = 0)
 
-print(longlatDF)
+adjustedLonglat = longlatDF.drop(['clon00', 'clat00', 'pclon00', 'pclat00', 'pclon10', 'pclat10'], axis = 1)
+
+print(adjustedLonglat)
 print(stateDF)
 print(countyDF)
 print(weatherDF)
 
-adjustedLonglat = longlatDF.drop(['clon10', 'clat10', 'pclon00', 'pclat00', 'pclon10', 'pclat10'], axis = 1)
-
-
-adjustedLonglat.to_sql("lat_long", con = engine, if_exists = "replace")
+adjustedLonglat.to_sql("lat_long", con = engine, if_exists = "replace", dtype = {"clon10": Numeric, "clat10": Numeric})
 engine.execute("SELECT * FROM lat_long").fetchall()
 print("lat_long table updated!")
 
@@ -36,3 +35,7 @@ print("state table updated!")
 countyDF.to_sql("county", con = engine, if_exists = "replace")
 engine.execute("SELECT * FROM county").fetchall()
 print("county table updated!")
+
+weatherDF.to_sql("weather", con = engine, if_exists = "replace", dtype = {"Latitude": Numeric, "Longitude": Numeric})
+engine.execute("SELECT * FROM weather").fetchall()
+print("weather table updated!")
